132a8552933d869dc600b1d1f1d30e30e5e3c996
copper <411998690@qq.com>
2026-02-12 21:22:29 +0800

    [feat]add new mod: BuildableWildPlant

---
+++ GGBuildableWildPlant/BuildableWildPlantConfig.cs
@@ -0,0 +1,51 @@
+﻿using STRINGS;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGGMod.BuildableWildPlant {
+    public class BuildableWildPlantConfig : IBuildingConfig {
+        public const string ID = "GgBuildableWildPlant";
+        private readonly static List<Tag> m_storageFilters = new List<Tag> { GameTags.Seed };
+        public override BuildingDef CreateBuildingDef() {
+            var buildingdef = BuildingTemplates.CreateBuildingDef(
+                ID, 1, 1, "ggplanttile_kanim", 30, 30f,
+                TUNING.BUILDINGS.CONSTRUCTION_MASS_KG.TIER4,
+                TUNING.MATERIALS.FARMABLE,
+                1600f,
+                BuildLocationRule.Anywhere,
+                noise: TUNING.NOISE_POLLUTION.NONE,
+                decor: TUNING.BUILDINGS.DECOR.NONE
+            );
+            buildingdef.Floodable = false;
+            buildingdef.Overheatable = false;
+            buildingdef.AudioCategory = "HollowMetal";
+            buildingdef.AudioSize = "small";
+            buildingdef.ConstructionOffsetFilter = BuildingDef.ConstructionOffsetFilter_OneDown;
+            buildingdef.DragBuild = true;
+            buildingdef.AddSearchTerms(SEARCH_TERMS.FOOD);
+            buildingdef.AddSearchTerms(SEARCH_TERMS.FARM);
+            return buildingdef;
+        }
+        public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag) {
+            GeneratedBuildings.MakeBuildingAlwaysOperational(go);
+            BuildingConfigManager.Instance.IgnoreDefaultKComponent(typeof(RequiresFoundation), prefab_tag);
+            Prioritizable.AddRef(go);
+            var storage = go.AddOrGet<Storage>();
+            storage.capacityKg = 1f;
+            storage.showInUI = true;
+            storage.allowItemRemoval = false;
+            storage.showDescriptor = true;
+            storage.storageFilters = m_storageFilters;
+            storage.storageFullMargin = TUNING.STORAGE.STORAGE_LOCKER_FILLED_MARGIN;
+            storage.fetchCategory = Storage.FetchCategory.GeneralStorage;
+            storage.showCapacityStatusItem = true;
+            storage.showCapacityAsMainStatus = true;
+            go.AddOrGet<CopyBuildingSettings>().copyGroupTag = GameTags.Seed;
+            go.AddOrGet<BuildableWildPlant>();
+        }
+
+        public override void DoPostConfigureComplete(GameObject go) {
+            GeneratedBuildings.RemoveLoopingSounds(go);
+        }
+    }
+}
---
+++ GGBuildableWildPlant/STRINGS.cs
@@ -0,0 +1,25 @@
+﻿using OUI = STRINGS.UI;
+
+namespace GGGMod.BuildableWildPlant {
+    public static class STRINGS {
+        public static class BUILDINGS {
+            public static class PREFABS {
+                public static class GGBUILDABLEWILDPLANT {
+                    public static LocString NAME = OUI.FormatAsLink("Buildable Wild Plant", BuildableWildPlantConfig.ID);
+                    public static LocString DESC = "A farm tile that can turn into dirt";
+                    public static LocString EFFECT = "After seeding the seeds here, the tile will turn into diry and seeds grow into plants";
+                }
+            }
+
+            public static class BUTTONS {
+                public static class HAULINGPOINT {
+                    // Auto-Drop
+                    public static LocString AUTO_PLANT_ON = "Enable Auto-Plant";
+                    public static LocString AUTO_PLANT_ON_TOOLTIP = "If enabled, automatically plant the seed when storage is full";
+                    public static LocString AUTO_PLANT_OFF = "Disable Auto-Plant";
+                    public static LocString AUTO_PLANT_OFF_TOOLTIP = "Cancel automatic planting when full";
+                }
+            }
+        }
+    }
+}
