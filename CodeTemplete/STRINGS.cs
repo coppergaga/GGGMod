b6ecf8b5bcf49a18283c02138b7c7afdad0ce3ea
copper <411998690@qq.com>
2026-02-12 21:19:19 +0800

    [feat]split sln to multi projects, Add code templete

---
+++ CodeTemplete/STRINGS.cs
@@ -0,0 +1,15 @@
+﻿using OUI = STRINGS.UI;
+
+namespace GGGMod.BuildableWildPlant {
+    public static class STRINGS {
+        public static class BUILDINGS {
+            public static class PREFABS {
+                public static class GGBUILDABLEWILDPLANT {
+                    //public static LocString NAME = OUI.FormatAsLink("Buildable Wild Plant", BuildingConfig.ID);
+                    //public static LocString DESC = "A farm tile that simulates wild conditions";
+                    //public static LocString EFFECT = "Any plants planted in it will be considered as wild plants";
+                }
+            }
+        }
+    }
+}
